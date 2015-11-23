54f3c74f6f5cfd70597dd56acf3d873a2f06bfcb
tsloveme <472960087@qq.com>
2015-11-24 01:50:55 +0800

    数据库修改、静态资源导入，基础数据功能搭建

--- thinkApp/Application/AreaSelect/Controller/IndexController.class.php
+++ thinkApp/Application/AreaSelect/Controller/IndexController.class.php
@@ -3,12 +3,29 @@ namespace AreaSelect\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        //echo(getcwd());
-        //print_r(pdo_drivers());
-        //echo('--------------');
-        //echo(C('DB_NAME'));
         $citys = M('province');
         $data = $citys->select();
+        //print_r($data);
+        $this->display();
+    }
+    //获取所有省份
+    public function getProvince(){
+        $city = M('Province');
+        $data = $city->select();
         $this->ajaxReturn($data);
     }
+    //获取某个省份的所有市
+    public function getCity($id){// /getCity?id=11 or /getCity/id/11
+        $city = M('City');
+        $data = $city->where('cityId='.$id)->select();
+        $this->ajaxReturn($data);
+    }
+    //获取某个市的所有区域
+    public function getZone($id){
+        $zone = M('Zone');
+        $data = $zone->where()->select();
+        $this->ajaxReturn($data);
+    }
+
+
 }
