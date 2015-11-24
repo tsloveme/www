2d36c8f2d3d58a4a62f8c98cef8a609e2e65cc64
teensun <teensun>
2015-11-24 12:52:17 +0800

    some change

---
+++ thinkApp/Application/Runtime/Cache/AreaSelect/32d11c07ea3ce410deb1ed9c043303b9.php
@@ -0,0 +1,23 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>select省市区联动</title>
+    <script src="/thinkapp/Public/js/jquery1.8.3.min.js"></script>
+    <script src="/thinkapp/Public/js/vselect.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/thinkapp/Public/css/vselect.css" />
+    <style>
+        body{padding:0 35px;}
+    </style>
+    <script>
+        $(function(){
+
+        })
+    </script>
+
+</head>
+<body>
+<H1>select省市区联动</H1>
+
+</body>
+</html>
